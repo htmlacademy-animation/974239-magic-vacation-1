e31b21bebf0f20b2d0104223dd91288bfa7421b5
Гриневич Александр Васильевич (4029681) <grinevich@dev.vtb.ru>
2021-12-14 12:24:04 +0300

    Анимация кнопок, смена темы анимация футера и появление кнопки

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,9 +1,41 @@
 import Swiper from "swiper";
 
+const slidesMeta = [
+  {
+    activeScreens: [0, 1],
+    gradient: `linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`,
+    image: `url("img/slide1.jpg")`,
+    bodyClass: `body--base`
+  }, {
+    activeScreens: [2, 3],
+    gradient: `linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`,
+    image: `url("img/slide2.jpg")`,
+    bodyClass: `body--light`
+  }, {
+    activeScreens: [4, 5],
+    gradient: `linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`,
+    image: `url("img/slide3.jpg")`,
+    bodyClass: `body--blue`
+  }, {
+    activeScreens: [6, 7],
+    gradient: `linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`,
+    image: `url("img/slide4.jpg")`,
+    bodyClass: `body--base`
+  }
+];
+
 export default () => {
   let storySlider;
   let sliderContainer = document.getElementById(`story`);
-  sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+
+  const onSlideChange = (withGradient) => () => {
+    const activeSlideMeta = slidesMeta.find(({activeScreens}) => activeScreens.includes(storySlider ? storySlider.activeIndex : 0));
+    sliderContainer.style.backgroundImage = [activeSlideMeta.image, withGradient && activeSlideMeta.gradient].filter(Boolean).join(`, `);
+    document.body.classList.remove(`body--base`, `body--light`, `body--blue`);
+    document.body.classList.add(activeSlideMeta.bodyClass);
+  };
+
+  onSlideChange(true)();
 
   const setSlider = function () {
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
@@ -16,17 +48,7 @@ export default () => {
           enabled: true
         },
         on: {
-          slideChange: () => {
-            if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
-              sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
-            } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
-              sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
-            } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
-              sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
-            } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
-              sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
-            }
-          },
+          slideChange: onSlideChange(true),
           resize: () => {
             storySlider.update();
           }
@@ -50,17 +72,7 @@ export default () => {
           enabled: true
         },
         on: {
-          slideChange: () => {
-            if (storySlider.activeIndex === 0) {
-              sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
-            } else if (storySlider.activeIndex === 2) {
-              sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
-            } else if (storySlider.activeIndex === 4) {
-              sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
-            } else if (storySlider.activeIndex === 6) {
-              sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
-            }
-          },
+          slideChange: onSlideChange(false),
           resize: () => {
             storySlider.update();
           }
